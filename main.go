1f010ac7fdc76678b8296244b277631d386cf8a5
nemusou <nemusou@gmail.com>
2019-12-09 22:24:35 +0900

    feat: parse args

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
   "fmt"
+  "flag"
 )
 
 func main() {
+  var username string = parseArgs()
   fmt.Println("Hello notifissue.")
   printIssues([]string{"hoge", "poyo"})
+}
+
+func parseArgs() string {
+  var username = flag.String("u", "", "username")
+  flag.Parse()
+  return *username
 }
 
 func printIssues(issues []string) {
