5182dbdb414ea10efc738ec420bcc164d9b56ea6
nemusou <nemusou@gmail.com>
2019-12-09 22:25:05 +0900

    feat: request to github api with username

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
   "fmt"
   "flag"
+  "net/http"
 )
 
 func main() {
   var username string = parseArgs()
   fmt.Println("Hello notifissue.")
   printIssues([]string{"hoge", "poyo"})
+  fmt.Println(fetchUserInfo(username))
 }
 
 func parseArgs() string {
@@ -21,4 +23,9 @@ func printIssues(issues []string) {
   for _, issue := range issues {
     fmt.Println(issue)
   }
+}
+
+func fetchUserInfo(username string) (resp *http.Response, err error) {
+  var url string = "https://api.github.com/users/" + username
+  return http.Get(url)
 }
