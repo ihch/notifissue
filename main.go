c1f46b89c99067f96c06fab30a319548c18a267a
nemusou <nemusou@gmail.com>
2019-12-09 22:12:20 +0900

    feat: print issues function

--- main.go
+++ main.go
@@ -6,4 +6,11 @@ import (
 
 func main() {
   fmt.Println("Hello notifissue.")
+  printIssues([]string{"hoge", "poyo"})
+}
+
+func printIssues(issues []string) {
+  for _, issue := range issues {
+    fmt.Println(issue)
+  }
 }
