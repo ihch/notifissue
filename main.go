ff46317e9550e8f97b69f73a77d57816ccd0c7c3
nemusou <ryubi.102395@gmail.com>
2019-12-10 23:11:00 +0900

    feat: fetch関数を定義した

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
   "fmt"
   "flag"
+  "log"
   "net/http"
+  "io/ioutil"
 )
 
 func main() {
   var username string = parseArgs()
   fmt.Println("Hello notifissue.")
   printIssues([]string{"hoge", "poyo"})
-  fmt.Println(fetchUserInfo(username))
+  body, err := fetchUserInfo(username)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(string(body))
 }
 
 func parseArgs() string {
@@ -25,7 +31,25 @@ func printIssues(issues []string) {
   }
 }
 
-func fetchUserInfo(username string) (resp *http.Response, err error) {
+func fetchUserInfo(username string) (body string, err error) {
   var url string = "https://api.github.com/users/" + username
-  return http.Get(url)
+  return fetch(url)
+}
+
+func fetch(url string) (body string, err error) {
+  var resp *http.Response
+  resp, err = http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer resp.Body.Close()
+
+  var bytes []byte
+  bytes, err = ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  body = string(bytes)
+  return body, err
 }
